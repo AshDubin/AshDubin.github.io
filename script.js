8a67d5b2ad335d2a969af521db6493c46fa94c5b
AshDubin <120677962+AshDubin@users.noreply.github.com>
2022-12-16 20:06:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function toggleSubMenu() {
 
   // Toggle the display of the sub menu
   if (subMenu.style.display === 'block') {
-    subMenu.style.display = 'none';
-  } else {
-    subMenu.style.display = 'block';
+     subMenu.style.display = 'none';
+   } else {
+     subMenu.style.display = 'block';
   }
 }
